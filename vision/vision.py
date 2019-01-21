9cf8c0289e4d4d70c5dc7b985538c24427894fa7
Daniel Zou <zoudl2000@gmail.com>
2019-01-20 22:19:13 -0600

    Vision for reflective targets

--- vision/vision.py
+++ vision/vision.py
@@ -1,55 +1,117 @@
 import cv2 as cv
 import numpy as np
 
-capture = cv.VideoCapture(1)
+WIDTH = 320
+HEIGHT = 240
 
-while True:
-    # Take each frame
-    _, frame = capture.read()
-
-    # Convert BGR to HSV
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-
-    # Define range of stripes in HSV
-    lower_target = np.array([30, 100, 200])
-    upper_target = np.array([40, 255, 255])
-
-    # Threshold the HSV image to get only desired color
-    mask_target = cv.inRange(hsv, lower_target, upper_target)
-    ret, filter_target = cv.threshold(cv.blur(mask_target, (5, 5)), 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
-
-    # Contours
-    contours, hierarchy = cv.findContours(filter_target, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-    result = cv.bitwise_and(frame, frame, mask=filter_target)
-
-    # Find target
-    if contours:
-        areas = [(i, cv.contourArea(cnt)) for i, cnt in enumerate(contours)]
-        found = 0
-        for area in areas:
-            if area[1] > 1000:
-                cv.drawContours(frame, [contours[area[0]]], 0, (0,255,0), 3)
-                found += 1
-            if found == 2:
-                break
-            
+def findTarget(frame, mask):
+    ret, contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     if contours:
-        areas = [(i, cv.contourArea(cnt)) for i, cnt in enumerate(contours)]
+        areas = {i: cv.contourArea(cnt) for i, cnt in enumerate(contours)}
+        areas = minValue(areas, 200)
+        if not areas:
+            return None
+
+        boxes = {}
+        for i, area in areas.items():
+            box = cv.boxPoints(cv.minAreaRect(contours[i]))
+            box = np.int0(box)
+            boxes[i] = list(box)
+            cv.drawContours(frame, [box], 0, (0, 255, 0), 2)
+
+        approxWidth = 0
+        for i, box in boxes.items():
+            box = sorted(box, key=lambda b : b[1])
+            top1 = box[0]
+            cv.circle(frame, (top1[0], top1[1]), 2, (255, 0, 0), -1)
+            top2 = box[1]
+            cv.circle(frame, (top2[0], top2[1]), 2, (0, 0, 255), -1)
+            approxWidth += distance(top1, top2)
+
+            if top1[0] < top2[0]:
+                boxes[i] = ('L', top2)
+            elif top1[0] > top2[0]:
+                boxes[i] = ('R', top2)
+            else:
+                boxes[i] = ('X')
+
+        approxWidth /= len(boxes)
+        targetPairs = []
+        leftTargets = [(i, box) for i, box in boxes.items() if box[0] == 'L']
+        rightTargets = [(i, box) for i, box in boxes.items() if box[0] == 'R']
+
+        for i, left in leftTargets:
+            for j, right in rightTargets:
+                if approx(distance(left[1], right[1]), approxWidth*4):
+                    targetPairs.append((left, right))
         
-        for area in areas:
-            if area[1] > 1000:
-                cv.putText(frame, str(area[1]), (0, 10), cv.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
-                cv.drawContours(frame, [contours[area[0]]], 0, (0,255,0), 3)
-                # box = cv.boxPoints(cv.minAreaRect(contours[0]))
-                # box = np.int0(box)
-                # cv.drawContours(frame, [box], 0, (0, 255, 0), 2)
-                break
+        midpoints = [midpoint(left[1], right[1]) for left, right in targetPairs]
+        if midpoints:
+            target = min(midpoints, key=lambda m : distance(m, (WIDTH/2, HEIGHT/2)))
+            cv.circle(frame, target, 4, (0, 255, 0), -1)
+            return target
 
-    cv.imshow('Sinks', np.hstack([frame, result]))
+def findHatch():
+    pass
 
-    if cv.waitKey(5) & 0xFF == 27:
-        break
+def findCargo():
+    pass
 
-cv.destroyAllWindows()
+
+########## UTIL ##########
+
+def minValue(dict, min):
+    return {i: value for i, value in dict.items() if value > min}
+
+def outliers(dict, min=100, s=2):
+    mean = np.mean(list(dict.values()))
+    std = np.std(list(dict.values()))
+    return {i: value for i, value in dict.items() if value-mean > s*std and value > min}
+
+def distance(tuple1, tuple2):
+    return ((tuple1[0] - tuple2[0])**2 + (tuple1[1] - tuple2[1])**2)**0.5
+
+def midpoint(tuple1, tuple2):
+    return ((tuple1[0] + tuple2[0]) // 2, (tuple1[1] + tuple2[1]) // 2)
+
+def approx(num1, num2, error=0.1):
+    return abs(num1-num2) / (num1+num2) <= error
+
+
+########## MAIN ##########
+
+def main():
+    kernel = np.ones((5, 5), np.uint8)
+
+    capture = cv.VideoCapture(0)
+    capture.set(3, 320)
+    capture.set(4, 240)
+
+    while 1:
+        if cv.waitKey(5) & 0xFF == 27:
+            break
+
+        ret, frame = capture.read()
+
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+        lowerTarget = np.array([90, 150, 80])
+        upperTarget = np.array([105, 255, 255])
+
+        maskTarget = cv.inRange(hsv, lowerTarget, upperTarget)
+        # maskTarget = cv.dilate(maskTarget, kernel, iterations = 1)
+        maskTarget = cv.morphologyEx(maskTarget, cv.MORPH_CLOSE, kernel)
+        # maskTarget = cv.GaussianBlur(maskTarget,(5,5),0)
+
+        target = findTarget(frame, maskTarget)
+
+        maskFrame = maskTarget
+        cv.imshow('Frame', frame)
+        cv.imshow('Masks', maskFrame)
+
+    capture.release()
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
